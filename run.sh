17a231c9a2bdec0cd35f5d298faf44389304e0dc
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-26 23:16:53 +0800

    n

---
+++ run.sh
@@ -0,0 +1,2 @@
+make -B \
+&& ./second_largest.exe
---
+++ second_largest.cc
@@ -0,0 +1,30 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include<iostream>
+
+using namespace std;
+
+void second_largest(int array[], int len) 
+{
+}
+
+void print_array(int array[], int len)
+{
+  for (int i=0;i<len;i++)
+  {
+    cout<<array[i]<<",";
+  }
+  cout<<endl;
+}
+
+int main() 
+{
+  int a[] = {4, 2, 5};
+  {1, 2, 3, 4} 3
+  {{4, 1, 2, 3}} 3
+  {1, 1, 2, 2} 1
+  {1, 1} -1
+  {1} -1
+  {} -1
+  return 0;
+}
