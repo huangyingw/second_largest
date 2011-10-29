03afaa41c301e5c09d365942634b4e84f0050d90
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-29 16:05:35 +0800

    n

--- second_largest.cc
+++ second_largest.cc
@@ -6,6 +6,21 @@ using namespace std;
 
 void second_largest(int array[], int len) 
 {
+  int max,sec_max;
+  max=sec_max=-1000;
+  for (int i=0;i<len;i++)
+  {
+    if (array[i]>max)
+    {
+      sec_max=max;
+      max=array[i];
+    }
+    else if( array[i] < max && array[i] > sec_max)
+    {
+      sec_max=array[i];
+    }
+  }
+  cout<<sec_max<<endl;
 }
 
 void print_array(int array[], int len)
@@ -20,11 +35,14 @@ void print_array(int array[], int len)
 int main() 
 {
   int a[] = {4, 2, 5};
-  {1, 2, 3, 4} 3
+  second_largest(a,sizeof(a)/sizeof(int));
+
+  /*{1, 2, 3, 4} 3
   {{4, 1, 2, 3}} 3
   {1, 1, 2, 2} 1
   {1, 1} -1
   {1} -1
   {} -1
+  */
   return 0;
 }
